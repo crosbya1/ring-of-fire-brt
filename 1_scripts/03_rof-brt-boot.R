b0caa513e4381ae1adb57dad527d02131d3bcda9
crosbya1 <crosby@ualberta.ca>
2022-02-26 07:56:55 -0700

    update brt boot

--- 1_scripts/03_rof-brt-boot.R
+++ 1_scripts/03_rof-brt-boot.R
@@ -11,13 +11,24 @@ library(segmented)
 load("0_data/processed/BAMv6_RoFpackage_2022-01.Rdata")
 
 
+rel_inf <- function(res) {
+  rel.inf <- relative.influence(res, res$n.trees)
+  rel.inf[rel.inf < 0] <- 0
+  i <- order(-rel.inf)
+  rel.inf <- 100 * rel.inf/sum(rel.inf)
+  out <- data.frame(var = res$var.names[i], rel.inf = rel.inf[i])
+  attr(out, "n.trees") <- res$n.trees
+  out
+}
+
+
 # Function to run bootstrapped BRT models
 run_brt1 <- function(i, spp) {
   RATE=0.001
-  #  u <- rel_inf(res)
-  #  vars <- c("count", "offset", as.character(u$var[u$rel.inf > 0]))
-  #  ntree <- res$n.trees
-  ntree <- 10000
+  u <- rel_inf(res)
+  vars <- c("count", "offset", as.character(u$var[u$rel.inf > 0]))
+  ntree <- res$n.trees
+  # ntree <- 10000
   si <- BB[,i]
   if (sum(y[si, spp]) < 1)
     return(structure(sprintf("0 detections for %s", spp), class="try-error"))
@@ -49,11 +60,12 @@ if(!dir.exists(dir)){dir.create(dir)}
 system.time({
   for (spp in SPP[1:2]) {
     cat("\n\n------------------------------", spp, "------------------------------\n\n")
+    load(paste0("2_pipeline/store/brt2-xv/", spp, ".Rdata"))
     d1 <- paste0("2_pipeline/store/brt-boot-1/", spp)
     if(!dir.exists(d1)){dir.create(d1)}
     for(i in 1:2){
-      res <- run_brt1(i, spp)
-      save(res, file=paste0("2_pipeline/store/brt-boot-1/", spp, "/", i, ".RData"))
+      res1 <- run_brt1(i, spp)
+      save(res1, file=paste0("2_pipeline/store/brt-boot-1/", spp, "/", i, ".RData"))
     }
   }
 })
